fb703f19f6a4d0934694d9f9e4a195fab5b8dacb
Matthew Melvin <matthewmelvin@gmail.com>
2011-11-08 00:10:01 +0000

    all twitter urls are in t.co now so url parster need not be so generous

--- ttytter.email.pl
+++ ttytter.email.pl
@@ -41,7 +41,9 @@ $handle = sub {
 
 	$orig = $text;
 	$text =~ s/\\[ntr]/ /g;
-	$text =~ s!(https?://([-\w\.]+)+(:\d+)?(/([\w/_\.\~\-\#\!,]*(\?\S+)?)?)?)!<a href="$1">$1</a>!g;
+	# $text =~ s!(https?://([-\w\.]+)+(:\d+)?(/([\w/_\.\~\-\#\!,]*(\?\S+)?)?)?)!<a href="$1">$1</a>!g;
+	# http://t.co/i7GwIev8 
+	$text =~ s!(https?://t\.co/[a-zA-Z0-9]{8})!<a href="$1">$1</a>!g;
 	$text =~ s/(^|\s+)#(\S+)/$1<a href="http:\/\/search.twitter.com\/search?q=$2">#$2<\/a>/g;
 	$text =~ s/(^|\s+|\.|")\@([a-zA-Z0-9_]{1,15})/$1<a href="http:\/\/twitter.com\/$2">\@$2<\/a>/g;
 
