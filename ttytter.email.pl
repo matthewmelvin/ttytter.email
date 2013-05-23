e848e8dc7f8a5bd72a058bf1fd2cb3b90c7d7cf4
Matthew Melvin <matthewmelvin@gmail.com>
2013-05-23 02:54:28 +0000

    updated for ttytter 2.1

--- ttytter.email.pl
+++ ttytter.email.pl
@@ -4,6 +4,7 @@ use MIME::Lite;
 use Date::Manip;
 use HTML::Entities;
 use LWP::UserAgent;
+use Data::Dumper;
 
 $MSGIDS = "$ENV{'HOME'}/.ttytter.thrdids";
 %MSGIDS = ();
@@ -14,6 +15,13 @@ if (open(F, "<$MSGIDS")) {
 	}
 	close(F);
 }
+
+
+$VER = do {
+        my @r = (q$Revision: 1.12 $ =~ /\d+/g);
+        sprintf "%d."."%02d", @r
+};
+
 print $stdout "" . scalar(keys %MSGIDS) . " \"Message-Id\" / \"References\" pairs loaded.\n";
 
 sub unredir($) {
@@ -46,7 +54,9 @@ $handle = sub {
 	my($msgid) = &descape($ref->{'id_str'});
 	my($thrdid) = &descape($ref->{'in_reply_to_status_id_str'});
 	my($subj) = "$name: $text";
-	my($mesg, $date, $orig);
+	my($mesg, $date, $orig, $url);
+
+	# print Dumper($ref);
 
 	if ($MSGIDS{$msgid}) {
 		$date = UnixDate($ref->{'created_at'}, "%H:%M:%S");
@@ -61,10 +71,15 @@ $handle = sub {
 	}
 	$MSGIDS{$msgid} = $thrdid;
 
+
 	$orig = $text;
 	$text =~ s/\\[ntr]/ /g;
 	# $text =~ s!(https?://([-\w\.]+)+(:\d+)?(/([\w/_\.\~\-\#\!,]*(\?\S+)?)?)?)!<a href="$1">$1</a>!g;
-	# http://t.co/i7GwIev8 
+	foreach (@{$ref->{'entities'}->{'urls'}}) {
+		$url = &descape($_->{'expanded_url'});
+		print $stdout "Searching for $url in $text ...\n";
+		$text =~ s!($url)!unredir($1)!eg;
+	}
 	$text =~ s!(https?://t\.co/[a-zA-Z0-9]*)!unredir($1)!eg;
 	$text =~ s/(^|\s+)#(\S+)/$1<a href="http:\/\/twitter.com\/search\/realtime\/$2">#$2<\/a>/g;
 	$text =~ s/(^|\s+|\.|")\@([a-zA-Z0-9_]{1,15})/$1<a href="http:\/\/twitter.com\/$2">\@$2<\/a>/g;
@@ -76,7 +91,9 @@ $handle = sub {
 		'Message-Id' => "<ttytter.$msgid\@$host>",
 		'References' => "<ttytter.$thrdid\@$host>",
 		'X-Psuedo-Feed-Url' => 'http://twitter.com',
+		'X-TTYtter-Email' => $VER,
 		'Data' => "<html><body>
+<!-- $subj -->
 <a href=\"http://twitter.com/$name\">$name</a>: $text</a>
 <p>URL: <a href=\"http://twitter.com/$name/statuses/$msgid\">http://twitter.com/$name/statuses/$msgid</a></p>
 </body></html>
@@ -86,7 +103,8 @@ $handle = sub {
 	$mesg->send();
 
 	$date = UnixDate($ref->{'created_at'}, "%H:%M:%S");
-	print $stdout "$date: $subj\n" if ( -t $stdout );
+	print $stdout "$date: subject: $subj\n" if ( -t $stdout );
+	print $stdout "$date: umodded: $name $orig\n" if ( -t $stdout );
 
 };
 
